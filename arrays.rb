8ab6e8b016628c421727d77bb059eb38c38eba8f
Asia <asiaellis5@gmail.com>
2019-12-09 17:07:28 +0000

    added arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,10 @@
+print [1, 2, 3, 4, 5].map! { |n| n + 1} 
+puts
+print [1, 3, 5, 4, 2].sort!
+puts
+print [1, 3, 5, 4, 2].sort!.map! { |n| n + 1}
+puts
+print [1, 2, 3, 4, 5].sum
+puts
+print [1, 2, 3, 4, 5].sum * 2
+
