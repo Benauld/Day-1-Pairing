a62a1560a1caef158bc36708a3cf0b42ced2e42a
benauld <ben@portobello-consultancy.co.uk>
2019-12-09 19:43:08 +0000

    Bens first commit with extra files

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,4 @@
-print [1, 2, 3, 4, 5].map! { |n| n + 1} 
+print [1, 2, 3, 4, 5].map! { |n| n + 1}
 puts
 print [1, 3, 5, 4, 2].sort!
 puts
@@ -7,4 +7,7 @@ puts
 print [1, 2, 3, 4, 5].sum
 puts
 print [1, 2, 3, 4, 5].sum * 2
-
+puts
+x = {a: 1, b: 2}
+print x.map { |key, value| [key, value += 1]}
+puts
